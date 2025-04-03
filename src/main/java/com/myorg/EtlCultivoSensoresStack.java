f0f1e8bb217669c52dad8e8ef6531406dd4f46e8
Enmanuel Ochoa <eochoa0007@gmail.com>
2025-04-03 04:18:23 -0500

    Inicio de proyecto cdk - se agregó la config del s3 - rds y vpc

--- src/main/java/com/myorg/EtlCultivoSensoresStack.java
+++ src/main/java/com/myorg/EtlCultivoSensoresStack.java
@@ -3,22 +3,33 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcLookupOptions;
 
 public class EtlCultivoSensoresStack extends Stack {
-    public EtlCultivoSensoresStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
-
     public EtlCultivoSensoresStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        // Bucket S3 para salida de datos procesados
+        Bucket bucketSalida = Bucket.Builder.create(this, "BucketSalidaCultivo")
+            .bucketName("datos-cultivo-procesados-" + this.getAccount())
+            .versioned(false)
+            .build();
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "EtlCultivoSensoresQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+
+        // RDS MySQL (usando tu VPC existente "vpc-32a04b48")
+        DatabaseInstance rdsSensores = DatabaseInstance.Builder.create(this, "RdsSensoresCultivo")
+            .engine(DatabaseInstanceEngine.mysql(MysqlInstanceEngineProps.builder()
+                .version(MysqlEngineVersion.VER_8_0)
+                .build()))
+            .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.SMALL))
+            .credentials(Credentials.fromGeneratedSecret("admin"))
+            .vpc(Vpc.fromLookup(this, "SensorCultivoVpc", VpcLookupOptions.builder()
+                .vpcId("vpc-32a04b48")
+                .build()))
+            .storageEncrypted(true)
+            .build();
     }
 }
